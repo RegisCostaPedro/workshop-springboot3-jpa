8a0938e17fbe1a8a9ff0844ce9353fb354652d8a
Regis Costa pedro <regiscostapedro0705@gmail.com>
2024-01-15 16:44:27 -0300

    Service layer, component registration

---
+++ src/main/java/com/regiscostapedro/course/services/UserService.java
@@ -0,0 +1,25 @@
+package com.regiscostapedro.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.regiscostapedro.course.entities.User;
+import com.regiscostapedro.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+	Optional<User> obj  =repository.findById(id);
+	return obj.get();
+	}
+}
