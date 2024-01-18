69bd0348f83fd0c14f3f3814fe0aa8529c5f8ee9
Regis Costa pedro <regiscostapedro0705@gmail.com>
2024-01-18 19:41:51 -0300

    Exception handling - update

--- src/main/java/com/regiscostapedro/course/services/UserService.java
+++ src/main/java/com/regiscostapedro/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.regiscostapedro.course.repositories.UserRepository;
 import com.regiscostapedro.course.services.exceptions.DatabaseException;
 import com.regiscostapedro.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	@Autowired
@@ -45,10 +47,15 @@ public class UserService {
 	
 	
 	public User update(Long id ,User obj) {
+		try {
 	User  entity = repository.getReferenceById(id);
 		updateData(entity, obj);
-	return repository.save(entity);
+	return repository.save(entity);}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
+	
 
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
